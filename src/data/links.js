a41a121746cbb1b86b948d4b020279c130cc8fa8
Ehimare369 <ehimare369@gmail.com>
2021-03-25 12:35:25 +0100

    Finished with the footer component

---
+++ src/data/links.js
@@ -0,0 +1,67 @@
+import { RiHomeHeartFill } from "react-icons/ri";
+
+export const navLinks = [
+  {
+    icon: <RiHomeHeartFill />,
+    label: "home",
+    url: "/",
+  },
+  {
+    icon: "",
+    label: "about",
+    url: "/about",
+  },
+  {
+    icon: "",
+    label: "services",
+    url: "/services",
+  },
+  {
+    icon: "",
+    label: "projects",
+    url: "/projects",
+  },
+  {
+    icon: "",
+    label: "blog",
+    url: "/blog",
+  },
+  {
+    icon: "",
+    label: "contact",
+    url: "/contact",
+  },
+];
+
+export const FOLLOW_LINKS = [
+  {
+    icon: "",
+    name: "tw",
+    url: "/",
+  },
+  {
+    icon: "",
+    name: "gh",
+    url: "/about",
+  },
+  {
+    icon: "",
+    name: "yt",
+    url: "/services",
+  },
+  {
+    icon: "",
+    name: "li",
+    url: "/projects",
+  },
+  {
+    icon: "",
+    name: "md",
+    url: "/blog",
+  },
+  {
+    icon: "",
+    name: "st",
+    url: "/contact",
+  },
+];
